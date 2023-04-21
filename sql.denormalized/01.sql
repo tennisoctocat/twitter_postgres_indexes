715a1c98bedca545f65be4a31faaea3004fc2ee5
Cynthia Hom <chom@g.hmc.edu>
2023-04-21 14:26:45 -0700

    added office hours suggestions and load tweets output

--- sql.denormalized/01.sql
+++ sql.denormalized/01.sql
@@ -15,6 +15,6 @@ WHERE jsonb_array_elements(
     COALESCE(data->'entities'->'hashtags','[]') ||
     COALESCE(data->'extended_tweet'->'entities'->'hashtags','[]')
 )->>'text'::TEXT @> 'coronavirus'::TEXT;*/ 
-
+/* Most likely issue with this is that postgres just thinks that the filter will be faster. So we should change the query (or index)? so that postgres will think that the index scan is faster. A hacky fix would be to do set set enable_seqscan=off; inside this file but that's probably not the best. */
 select count(distinct data->>'id') FROM tweets_jsonb
 WHERE data->'entities' @> '{"hashtags": [{"text": "coronavirus"}]}' or data->'extended_tweet'->'entities' @> '{"hashtags": [{"text": "coronavirus"}]}';
--- sql.denormalized/04.sql
+++ sql.denormalized/04.sql
@@ -23,4 +23,4 @@ AND data->'lang' ? 'en';
 /*CREATE INDEX ON tweets USING gin(to_tsvector('english', text));
 to_tsvector('english', COALESCE(data->'extended_tweet'->>'full_text',data->>'text'))
 */
-
+/* Current issue: only returns 6056 instead of 6058, fix is to tell gin to return more heap tuples. set gin_fuzzy_search_limit = 1000; */
